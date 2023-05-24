e9c096e1ebd860dbcb210bd22e36ca444aed2817
alisson98sb <alisson98sb@gmail.com>
2023-05-24 14:30:35 -0300

    First project - Hello world

---
+++ src/class1/Example.java
@@ -0,0 +1,13 @@
+package class1;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+
+//psv means our main method
+//sout means I wanto to print something
+//O nome da classe começa com letra maiuscula
+//O nome do método começa com letra minuscula
+//sout + tab = printf or console.log
---
+++ src/class1/Test.java
@@ -0,0 +1,7 @@
+package class1;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Java is so important language for programmer");
+    }
+}
