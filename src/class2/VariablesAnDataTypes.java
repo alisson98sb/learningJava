5958835ef3549f2f80d047a5f9e66196e826e707
alisson98sb <alisson98sb@gmail.com>
2023-05-24 22:04:29 -0300

    Variables types, Strings, class import, scanner

---
+++ src/class2/VariablesAnDataTypes.java
@@ -0,0 +1,49 @@
+package class2;public class VariablesAnDataTypes {
+    public static void main(String[] args) {
+        System.out.println("Variables - number");
+        /*
+         *   int (number, 4bytes)
+         *   double (float number, 8 bytes)
+         *   byte (numeric 1 byte)
+         *   short (number, 2 byte)
+         *   long (number, 8 bytes)
+         *   float (float number, 4 bytes)
+         *   char (character, 2 bytes)
+         *   boolean (true or false 1byte)
+         */
+
+        int my_age = 25;
+        int my_number = 1938403840;
+
+        short a = 5;
+        short cat = 99;
+
+        byte b = 77;
+        byte apple = 85;
+
+        long l = 1000;
+        long almond = 56;
+
+        float f = 23;
+        float geek = 88;
+
+        double d = 4;
+        double dl = 5.4;
+
+        char c = 'B';
+        boolean is_true = false;
+        boolean tt = true;
+
+        System.out.println(my_age);
+        System.out.println(my_number);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(apple);
+        System.out.println(l);
+        System.out.println(almond);
+        System.out.println(f);
+        System.out.println(geek);
+        System.out.println(d);
+        System.out.println(dl);
+    }
+}
